969473f5a0ecb80653aa512c47d2c7cc2a60f042
hjelmare <hjelmare@users.noreply.github.com>
2015-02-05 14:50:46 +0100

    nu måste vi bli klara snart va?

--- CalculateSubProblem.m
+++ CalculateSubProblem.m
@@ -9,14 +9,17 @@ h = sum(pi);
 %to why we have done this is that x_{s_l t_l l}=0 and x_{jil} = 0 when we
 %did not accept the path for pair l. This means that we do not have to
 %calculate these in the for-loop.
-temp = 0;
 for l = 1:nbrOfAcceptedPaths
+    temp = 0;
     for i = 1:n
         for j = 1:n
-            temp = temp * pi(i)*x(j, i, okcom(l));
+            temp = temp + pi(i)*x(j, i, okcom(l));
         end
     end
     
     h = h + 1 - temp;
 end
 
+
+
+end
